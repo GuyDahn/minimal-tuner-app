e9787dbf071158b875af934fa5479e6373f8ddce
G. Dane <guydaane@gmail.com>
2020-06-07 20:10:42 +0300

    added rangeBar logic

--- src/components/RangeBar/index.js
+++ src/components/RangeBar/index.js
@@ -5,6 +5,23 @@ import SemiCircleProgressBar from 'react-progressbar-semicircle'
 
 export default class RangeBar extends Component {
     render() {
-        
+        let { value } = this.props;
+        if (-5 <= value && value <= 5) {
+            value = 50
+        } else if (value < 0) {
+            value = 50 - Math.abs(value)
+        } else {
+            value = balue + 50
+        }
+
+        return (
+            <Container>
+                <SemiCircleProgressBar
+                    percentage={value}
+                    background={"#282c34"}
+                    orientation={"down"}
+                />
+            </Container>
+        )
     }
 }
